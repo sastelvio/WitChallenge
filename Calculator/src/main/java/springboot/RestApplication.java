bbcab4e5489c8a8ec96820a4a22d15724635fa7f
sastelvio <41056057+sastelvio@users.noreply.github.com>
2021-06-20 23:17:36 +0200

    Add files via upload

---
+++ Calculator/src/main/java/springboot/RestApplication.java
@@ -0,0 +1,17 @@
+package springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author Sastelvio Serafim Manuel
+ */
+@SpringBootApplication
+public class RestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApplication.class, args);
+	}
+
+}
---
+++ Calculator/src/main/java/springboot/ServletInitializer.java
@@ -0,0 +1,15 @@
+package springboot;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+/**
+ *
+ * @author Sastelvio Serafim Manuel
+ */
+public class ServletInitializer {
+
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(RestApplication.class);
+	}
+
+}
